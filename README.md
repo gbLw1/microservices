90e60cc6071bf4c2e202aae5cda176a269feddd3
Gabriel Grassi <gabrielhenrique2504@gmail.com>
2024-11-09 06:19:38 -0300

    feat: Controller and Actions

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # microservices
+
+## Testing
+
+To keep it simples, we are going to test the application endpoints using http rather than https for the most part for various reasons, more especially when you come to kubernetes, it becomes very complex and it's going to detract from the main focus of the course.
---
+++ src/PlatformService/Controllers/PlatformsController.cs
@@ -0,0 +1,54 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PlatformService.Data;
+using PlatformService.Dtos;
+using PlatformService.Models;
+
+namespace PlatformService.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class PlatformsController(
+    IPlatformRepo repository,
+    IMapper mapper) : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetPlatforms()
+    {
+        Console.WriteLine("--> Getting Platforms from PlatformService");
+
+        var platforms = repository.GetAllPlatforms();
+
+        return Ok(mapper.Map<IEnumerable<PlatformReadDto>>(platforms));
+    }
+
+    [HttpGet("{platformId}", Name = nameof(GetPlatformById))]
+    public IActionResult GetPlatformById(int platformId)
+    {
+        var platform = repository.GetPlatformById(platformId);
+
+        if (platform is null)
+        {
+            return NotFound();
+        }
+
+        return Ok(mapper.Map<PlatformReadDto>(platform));
+    }
+
+    [HttpPost]
+    public IActionResult CreatePlatform(PlatformCreateDto platformCreateDto)
+    {
+        var platformModel = mapper.Map<Platform>(platformCreateDto);
+
+        repository.CreatePlatform(platformModel);
+        repository.SaveChanges();
+
+        var platformReadDto = mapper.Map<PlatformReadDto>(platformModel);
+
+        return CreatedAtRoute(
+            routeName: nameof(GetPlatformById),
+            routeValues: new { platformId = platformReadDto.Id },
+            value: platformReadDto
+        );
+    }
+}
